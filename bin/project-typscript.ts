1747d451cad7e249b22f281b627e5d3bf87d192c
EC2 Default User <ec2-user@ip-172-31-22-187.ec2.internal>
2021-05-09 10:59:05 +0000

    Initial commit

--- bin/project-typscript.ts
+++ bin/project-typscript.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
 import { ProjectTypscriptStack } from '../lib/project-typscript-stack';
+const envUSA = { account: '326703389397', region: 'us-east-1' };
 
 const app = new cdk.App();
-new ProjectTypscriptStack(app, 'ProjectTypscriptStack');
+new ProjectTypscriptStack(app, 'ProjectTypscriptStack',{env: envUSA});
--- lib/project-typscript-stack.ts
+++ lib/project-typscript-stack.ts
@@ -1,18 +1,23 @@
 import * as sns from '@aws-cdk/aws-sns';
 import * as subs from '@aws-cdk/aws-sns-subscriptions';
 import * as sqs from '@aws-cdk/aws-sqs';
+import * as ec2 from '@aws-cdk/aws-ec2';
 import * as cdk from '@aws-cdk/core';
 
 export class ProjectTypscriptStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'ProjectTypscriptQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const vpc = new ec2.Vpc(this, 'CDKVPC', {
+      cidr: "10.0.0.0/16",
+      maxAzs: 2,
+      natGateways: 0,
     });
-
-    const topic = new sns.Topic(this, 'ProjectTypscriptTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    
+    const ec2instance = new ec2.Instance(this,'CDKEC2Instance', {instanceType:new ec2.InstanceType('t2.micro'),
+      machineImage:new ec2.LookupMachineImage({name:'amzn2-ami-hvm-2.0.20210421.0-x86_64-gp2'}),
+      vpc: vpc
+    })
+    
   }
 }
